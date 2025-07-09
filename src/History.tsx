c058d404ba9a30da2f0c9e403bb5cfad6efd988e
Oliver Hamilton <Oliver.Hamilton@softwire.com>
2025-07-09 17:28:46 +0100

    Browser routes implemented

--- src/History.tsx
+++ src/History.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function History(): React.ReactElement {
-    return <div>Hello world</div>
+    return <div>Hello world</div>;
 }
 
 export default History;
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import History from './History';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
